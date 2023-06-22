c68dc61e65101f2b7e55f54bb1b81fdf458ee14f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-22 12:04:01 -0500

    [+] product-box.component: Condiciona y cambia el despliegue el card de producto en una columna [ngClass]

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product-box',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-box.component.css']
 })
 export class ProductBoxComponent {
+  @Input() fullWidthMode = false;
 
 }
