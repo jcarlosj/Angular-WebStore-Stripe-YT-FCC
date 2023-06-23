1619b17a9d5fe0edfd27e158426550e5bd5bdb14
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-23 12:53:10 -0500

    [+] cart.component: Ajusta CSS del componente

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -7,13 +7,15 @@ import { Cart, CartItem } from '../../models/cart.model';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-  // Atributes
+  // Data estatica a desplegar
   cart: Cart = {
     items: [
       { id: 1, urlProduct: 'https://placehold.co/150x200', name: 'snikers', price: 150, quantity: 1 },
       { id: 2, urlProduct: 'https://placehold.co/150x200', name: 'puma', price: 175, quantity: 3 },
     ]
   }
+
+  // Datos que se van a desplegar en el FrontEnd
   dataSource: Array<CartItem> = [];
 
   // Definicion de cada una de las columnas desplegadas en el <ngContainer>
@@ -27,9 +29,10 @@ export class CartComponent implements OnInit {
 ];    // Si alguna de las columnas no esta aqui definida no se nuestra aunque este definida en el FrontEnd dentro de su <ng-container>
 
   ngOnInit() : void {
-    this.dataSource = this.cart.items;
+    this.dataSource = this.cart.items;    // Inicializa los datos a desplegar en la tabla en el FrontEnd
   }
 
+  // Itera cada uno de los items de producto para calcular el valor total de los productos en el carrito
   getTotal( items: Array<CartItem> ) : number {
     return items.map( item => item.price * item.quantity )
                 .reduce( ( prev, curr ) => prev + curr, 0 );
