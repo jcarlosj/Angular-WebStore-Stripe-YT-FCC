b88274b4fd8c02ce990c3f1eff55cc7d509b5907
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-23 12:25:38 -0500

    [+] cart.component: Despliega columna total por producto y total general

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -10,16 +10,28 @@ export class CartComponent implements OnInit {
   // Atributes
   cart: Cart = {
     items: [
-      { id: 1, urlProduct: 'https://placehold.co/150x200', name: 'snikers', price: 150, quantity: 3 }
+      { id: 1, urlProduct: 'https://placehold.co/150x200', name: 'snikers', price: 150, quantity: 1 },
+      { id: 2, urlProduct: 'https://placehold.co/150x200', name: 'puma', price: 175, quantity: 3 },
     ]
   }
   dataSource: Array<CartItem> = [];
 
   // Definicion de cada una de las columnas desplegadas en el <ngContainer>
-  displayedColumns: Array<string> = [ 'product', 'name', 'price', 'quantity' ];    // Si alguna de las columnas no esta aqui definida no se nuestra aunque este definida en el FrontEnd dentro de su <ng-container>
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total'
+];    // Si alguna de las columnas no esta aqui definida no se nuestra aunque este definida en el FrontEnd dentro de su <ng-container>
 
   ngOnInit() : void {
     this.dataSource = this.cart.items;
+  }
+
+  getTotal( items: Array<CartItem> ) : number {
+    return items.map( item => item.price * item.quantity )
+                .reduce( ( prev, curr ) => prev + curr, 0 );
   }
 
 }
