b939af7b736aa8de8f1521c75f1279e76b62e3fb
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-25 11:43:04 -0500

    [+] product.model [+] product-box.component: Crea Evento 'addToCart' para emitir valores al elemento padre usando @Output [+] home.component: Obtiene valores emitidos por el componente hijo vinculandolo a traves del metodo 'onAddToCart' al Servicio

--- src/app/pages/home/components/product-box/product-box.component.ts
+++ src/app/pages/home/components/product-box/product-box.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
 
 @Component({
   selector: 'app-product-box',
@@ -8,4 +9,18 @@ import { Component, Input } from '@angular/core';
 export class ProductBoxComponent {
   @Input() fullWidthMode = false;
 
+  product: Product | undefined = {
+    id: 1,
+    title: 'snikers',
+    price: 150,
+    category: 'shoes',
+    description: 'Description of product',
+    image: 'https://placehold.co/150x200'
+  };
+
+  @Output() addToCart = new EventEmitter();     // Creamos un evento llamado 'addToCard' al componente padre
+
+  onAddToCart() : void {
+    this.addToCart.emit( this.product );        // Agregamos los datos que vamos a emitir
+  }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+import { CartService } from 'src/app/services/cart.service';
 
 const ROWS_HEIGHT : { [id: number] : number } = {
   1: 370,   // 1: 400,
@@ -16,6 +18,8 @@ export class HomeComponent {
   rowHeight = ROWS_HEIGHT[ this.columnsToShow ];
   showCategory: string | undefined;
 
+  constructor( private cartService: CartService ) {}
+
   onColumnsToShow( numberCols: number ) : void {
     this.columnsToShow = numberCols;
     this.rowHeight = ROWS_HEIGHT[ this.columnsToShow ];
@@ -23,5 +27,16 @@ export class HomeComponent {
 
   onShowCategory( newCategory: string ) : void {
     this.showCategory = newCategory;
+  }
+
+  onAddToCart( product: Product ) : void {
+    // CartItem = Product [Entities/Model]
+    this.cartService.addToCart({
+      id: product.id,
+      urlProduct: product.image,
+      name: product.title,
+      price: product.price,
+      quantity: 1
+    });
   }
 }
