17c0ce5c37c1dcd857128c9b239e35b561f4c940
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-06-25 14:03:43 -0500

    [+] header.component: Getter/Setter de items en el carrito & Conecta con el Service para obtener el total de productos del carrito y eliminar todos sus items [+] app.component: recibe del componente hijo los datos del carrito y se subscribe al service para obtener su data

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, Input, ViewChild } from '@angular/core';
 import { MatMenuTrigger } from '@angular/material/menu';
+
+import { Cart, CartItem } from 'src/app/models/cart.model';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -8,6 +11,33 @@ import { MatMenuTrigger } from '@angular/material/menu';
 })
 export class HeaderComponent {
   @ViewChild( MatMenuTrigger ) trigger!: MatMenuTrigger;
+
+  private _cart: Cart = { items: [] };
+  itemsQuantity: number = 0;
+
+  @Input()
+  get cart(): Cart {
+    return this._cart;
+  }
+  set cart( cart: Cart ) {
+    this._cart = cart;
+
+    // Obtenemos una lista con todas las cantidades de los productos
+    this.itemsQuantity = cart.items
+                            .map( item => item.quantity )
+                            .reduce( ( prev, curr ) => prev + curr, 0 );
+  }
+
+  constructor( private cartService: CartService ) {}
+
+  // Itera cada uno de los items de producto para calcular el valor total de los productos en el carrito usando el service
+  getTotal( items: Array<CartItem> ) : number {
+    return this.cartService.getTotal( items );
+  }
+
+  onClearCart() : void {
+    this.cartService.clearCart();
+  }
 
   someMethod() {
     this.trigger.openMenu();
